0d8670244e99a0dd4a7db513a445cf7df3428fd8
yls <1329926763@qq.com>
2020-12-06 20:51:44 +0800

    '1'

--- src/main/java/com/example/blog/controller/BlogController.java
+++ src/main/java/com/example/blog/controller/BlogController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/blog")
 public class BlogController {
 
     private final BlogService blogService;
